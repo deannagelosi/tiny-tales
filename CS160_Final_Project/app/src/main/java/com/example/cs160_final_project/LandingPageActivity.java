fe854c80aeddf6e9d5d37e38ccf50156536ca130
lucylanlou <lucy.lou@berkeley.edu>
2020-11-21 14:02:21 -0800

    center crop

--- CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/LandingPageActivity.java
+++ CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/LandingPageActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -85,9 +87,9 @@ public class LandingPageActivity extends Activity {
         allStoriesList.get(2).add(BitmapFactory.decodeResource(this.getResources(), R.drawable.filler_image_3));
 
         //Setting the option images
-        topLeftImage.setImageBitmap(allStoriesList.get(0).get(0));
-        topRightImage.setImageBitmap(allStoriesList.get(1).get(0));
-        bottomLeftImage.setImageBitmap(allStoriesList.get(2).get(0));
+        topLeftImage.setImageBitmap(scaleCenterCrop(allStoriesList.get(0).get(0), 170, 170));
+        topRightImage.setImageBitmap(scaleCenterCrop(allStoriesList.get(1).get(0), 10, 170));
+        bottomLeftImage.setImageBitmap(scaleCenterCrop(allStoriesList.get(2).get(0), 170, 170));
 
 
         addPhotosButton.setOnClickListener(new View.OnClickListener() {
@@ -151,6 +153,39 @@ public class LandingPageActivity extends Activity {
                 startActivity(intent);
             }
         });
+    }
+
+    private Bitmap scaleCenterCrop(Bitmap source, int newHeight, int newWidth) {
+        int sourceWidth = source.getWidth();
+        int sourceHeight = source.getHeight();
+
+        // Compute the scaling factors to fit the new height and width, respectively.
+        // To cover the final image, the final scaling will be the bigger
+        // of these two.
+        float xScale = (float) newWidth / sourceWidth;
+        float yScale = (float) newHeight / sourceHeight;
+        float scale = Math.max(xScale, yScale);
+
+        // Now get the size of the source bitmap when scaled
+        float scaledWidth = scale * sourceWidth;
+        float scaledHeight = scale * sourceHeight;
+
+        // Let's find out the upper left coordinates if the scaled bitmap
+        // should be centered in the new size give by the parameters
+        float left = (newWidth - scaledWidth) / 2;
+        float top = (newHeight - scaledHeight) / 2;
+
+        // The target rectangle for the new, scaled version of the source bitmap will now
+        // be
+        RectF targetRect = new RectF(left, top, left + scaledWidth, top + scaledHeight);
+
+        // Finally, we create a new bitmap of the specified size and draw our new,
+        // scaled bitmap onto it.
+        Bitmap dest = Bitmap.createBitmap(newWidth, newHeight, source.getConfig());
+        Canvas canvas = new Canvas(dest);
+        canvas.drawBitmap(source, null, targetRect, null);
+
+        return dest;
     }
 }
 
