318cb2473de2274a00aa25c07dcd6b22fff65009
lucylanlou <lucy.lou@berkeley.edu>
2020-12-03 16:51:35 -0800

    dropdown

--- CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/ListenActivity.java
+++ CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/ListenActivity.java
@@ -1,13 +1,18 @@
 package com.example.cs160_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ListenActivity extends AppCompatActivity {
 
@@ -16,6 +21,13 @@ public class ListenActivity extends AppCompatActivity {
 
     private RecyclerView videoGrid;
 
+    public static RelativeLayout renamePopup;
+    public static RelativeLayout confirmationPopup;
+    public static CardView confirmButton;
+    public static CardView cancelButton;
+    public static EditText titleEditText;
+    public static Button doneButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +35,14 @@ public class ListenActivity extends AppCompatActivity {
 
         createTab = findViewById(R.id.createTab);
         videoGrid = findViewById(R.id.videoGrid);
+        renamePopup = findViewById(R.id.renamePopup);
+        confirmationPopup = findViewById(R.id.confirmationPopup);
+        confirmButton = findViewById(R.id.confirm);
+        cancelButton = findViewById(R.id.cancel);
+        titleEditText = findViewById(R.id.titleTextEdit);
+        doneButton = findViewById(R.id.doneButton);
 
-        videoGrid.setAdapter(new ListenPageRecycleViewAdapter(RecordingActivity.savedVideosList));
+        videoGrid.setAdapter(new ListenPageRecycleViewAdapter(this, RecordingActivity.savedVideosList));
         videoGrid.setLayoutManager(new GridLayoutManager(ListenActivity.this, 2));
 
         createTab.setOnClickListener(new View.OnClickListener() {
--- CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/ListenPageRecycleViewAdapter.java
+++ CS160_Final_Project/app/src/main/java/com/example/cs160_final_project/ListenPageRecycleViewAdapter.java
@@ -1,19 +1,35 @@
 package com.example.cs160_final_project;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import static com.example.cs160_final_project.ListenActivity.cancelButton;
+import static com.example.cs160_final_project.ListenActivity.confirmButton;
+import static com.example.cs160_final_project.ListenActivity.confirmationPopup;
+import static com.example.cs160_final_project.ListenActivity.doneButton;
+import static com.example.cs160_final_project.ListenActivity.renamePopup;
+import static com.example.cs160_final_project.ListenActivity.titleEditText;
+
 public class ListenPageRecycleViewAdapter extends RecyclerView.Adapter<ListenPageRecycleViewAdapter.ViewHolder> {
 
     private ArrayList<SavedVideo> videosList;
+    private Context context;
 
     /**
      * Provide a reference to the type of views that you are using
@@ -21,15 +37,17 @@ public class ListenPageRecycleViewAdapter extends RecyclerView.Adapter<ListenPag
      */
     public class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView videoImage;
-        private TextView storyTitle;
+        private EditText storyTitle;
         private ImageView videoOptions;
+        private ImageView storyOptions;
 
         public ViewHolder(View view) {
             super(view);
 
             videoImage = view.findViewById(R.id.videoImage);
             storyTitle = view.findViewById(R.id.storyTitle);
-            videoOptions = view.findViewById((R.id.storyOptions));
+            videoOptions = view.findViewById(R.id.storyOptions);
+            storyOptions = view.findViewById(R.id.storyOptions);
 
             videoImage.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
@@ -61,8 +79,9 @@ public class ListenPageRecycleViewAdapter extends RecyclerView.Adapter<ListenPag
     /**
      * Initialize the dataset of the Adapter.
      */
-    public ListenPageRecycleViewAdapter(ArrayList<SavedVideo> listOfVideos) {
+    public ListenPageRecycleViewAdapter(Context context, ArrayList<SavedVideo> listOfVideos) {
         videosList = listOfVideos;
+        this.context = context;
     }
 
     // Create new views (invoked by the layout manager)
@@ -80,6 +99,53 @@ public class ListenPageRecycleViewAdapter extends RecyclerView.Adapter<ListenPag
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         viewHolder.getVideoImageView().setImageBitmap(videosList.get(position).getCoverImage());
         viewHolder.getStoryTitleTextView().setText(videosList.get(position).getTitle());
+        viewHolder.storyOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //creating a popup menu
+                PopupMenu popup = new PopupMenu(context, viewHolder.storyOptions );
+                //inflating menu from xml resource
+                popup.inflate(R.menu.dropdown_menu);
+                //adding click listener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.rename:
+                                renamePopup.setVisibility(View.VISIBLE);
+
+                                doneButton.setOnClickListener(new View.OnClickListener() {
+                                    public void onClick(View v) {
+                                        viewHolder.storyTitle.setText(titleEditText.getText().toString());
+                                        renamePopup.setVisibility(View.GONE);
+                                    }
+                                });
+                                break;
+                            case R.id.delete:
+                                confirmationPopup.setVisibility(View.VISIBLE);
+                                confirmButton.setOnClickListener(new View.OnClickListener(){
+                                public void onClick(View v) {
+                                    videosList.remove(viewHolder.getAdapterPosition());
+                                    notifyItemRemoved(viewHolder.getAdapterPosition());
+                                    notifyItemRangeChanged(viewHolder.getAdapterPosition(), videosList.size());
+                                    confirmationPopup.setVisibility(View.GONE);
+                                }
+                            });
+                            cancelButton.setOnClickListener(new View.OnClickListener() {
+                                public void onClick(View v) {
+                                    confirmationPopup.setVisibility(View.GONE);
+                                }
+                            });
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -87,4 +153,5 @@ public class ListenPageRecycleViewAdapter extends RecyclerView.Adapter<ListenPag
     public int getItemCount() {
         return videosList.size();
     }
+
 }
